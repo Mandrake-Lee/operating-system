4b3b0411e949e91035f9af3a892d186f56e4c68e
Jorge Amorós-Argos <joramar76@gmail.com>
2021-04-16 16:06:56 +0000

    This commit adds a very initial support for Hassos + visual interface (Cog via DRM or Weston) on the top of the ComfilePi B070WR.
    The Comfile is basically a Raspberry CM3+ driving very known hardware, most of it already sold by Adrafruit or other dealers.
    
    For further information, see http://comfilewiki.co.kr/en/doku.php?id=comfilepi:index
    
    The present status is that the browser (from now on COG) can be launched but it is useless since the touchscreen is not recognised. Also a onscreen keyboard is missing.
    
    This must be refined. At this stage, version of COG is 0.8.0. As we can see, cog is moving rapidly to 0.10 and many of this issues seem to be solved.
    
    In the TODO list, although not exhaustive:
    * Launch COG as service or as a docker
    * Investigate the usage of present version of COG or pull ahead development version 0.9.2
    * Prepare support for the i2c buses in modules.conf
    * Prepare integration in Home Assistant with expansion board ComfilePi CP-IO22
    * Support buzzer with touchscreen
    * Clean unnecesary packages; most probably Weston will be dropped in favour of DRM

---
+++ buildroot-external/board/raspberrypi/cm3-B070WR-64/README.txt
@@ -0,0 +1,1 @@
+dt-blob.bin is taken from https://www.comfile.co.kr/download/pi/dt-blob.bin
--- buildroot-external/board/raspberrypi/hassos-hook.sh
+++ buildroot-external/board/raspberrypi/hassos-hook.sh
@@ -24,6 +24,32 @@ function hassos_pre_image() {
     if [[ "${BOARD_ID}" =~ "64" ]]; then
         sed -i "s|#arm_64bit|arm_64bit|g" "${BOOT_DATA}/config.txt"
     fi
+
+    # Here comes the tuning for the different CM3 boards/variants
+    if [[ "${BOARD_ID}" =~ "B070WR" ]]; then
+
+        #Copy dt-blob.bin in order to enable GPIO's during boot. This is a must!
+        cp "${BOARD_DIR}/dt-blob.bin" "${BOOT_DATA}/"
+
+        #Enable config.txt with all peripherals
+        sed -i "s|#arm_64bit|arm_64bit|g" "${BOOT_DATA}/config.txt"
+        sed -i "s|#hdmi_force_hotplug=1|hdmi_force_hotplug=1|g" "${BOOT_DATA}/config.txt"
+        sed -i "/#hdmi_mode=1/a hdmi_group=2\nhdmi_mode=87\nhdmi_cvt 800 480 60 6 0 0 0" "${BOOT_DATA}/config.txt"
+        sed -i "s|#dtparam=i2c_arm=on|dtparam=i2c_arm=on|g" "${BOOT_DATA}/config.txt"
+        sed -i "/#dtparam=spi=on/a dtparam=i2c2_iknowwhatimdoing\ndtparam=uart0=on" "${BOOT_DATA}/config.txt"
+        sed -i "/\/boot\/overlays\/README/a enable_uart=1\ndtoverlay=uart1,txd1_pin=32,rxd1_pin=33\ncore_freq=250\n#dtoverlay=i2c-gpio,i2c_gpio_sda=0,i2c_gpio_scl=1\ndtoverlay=pwm-2chan,pin=40,func=4,pin2=41,func2=4\ndtoverlay=i2c-rtc,ds3231" "${BOOT_DATA}/config.txt"
+        sed -i "/dtparam=audio=on/a desired_osc_freq=3700000" "${BOOT_DATA}/config.txt"
+        sed -i "s|#dtoverlay=vc4-fkms-v3d|dtoverlay=vc4-fkms-v3d|g" "${BOOT_DATA}/config.txt"
+
+        echo "i2c_dev" > "${TARGET_DIR}/etc/modules-load.d/modules.conf"
+
+        # Add serial console in cmd options
+        sed -i "s|$| console=serial0,115200|g" "${BOOT_DATA}/cmdline.txt"
+
+        # Add verbose output during early boot
+        sed -i -e "s/BOOT_UART=0/BOOT_UART=1/" "${BOOT_DATA}/bootcode.bin"
+
+    fi
 }
 
 
